447d0031fcaebaed54430893875df835022bd159
Subodh <bhattasub1045@gmail.com>
2025-12-31 13:32:57 +0545

    added

--- courier_management/courier/models.py
+++ courier_management/courier/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class cours(models.Model):
     name=models.CharField(max_length=200)
     phone=models.CharField(max_length=12)
+    address=models.CharField(max_length=34)
     
     def __str__(self):
         return self.name
