dc7dceefa68f0610e755940c1916226451596574
Sushil Bhatta <bhattsushil57@gmail.com>
2026-01-04 21:00:57 +0545

    managing routing

---
+++ Courier_Frontendd/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div style={{
+      display: "flex",
+      gap: "20px",
+      padding: "15px",
+      background: "#1e293b",
+      color: "white"
+    }}>
+      <Link to="/" style={{ color: "white" }}>Home</Link>
+      <Link to="/about" style={{ color: "white" }}>About</Link>
+      <Link to="/admin" style={{ color: "white" }}>Admin</Link>
+      <Link to="/staff" style={{ color: "white" }}>Staff</Link>
+      <Link to="/customer" style={{ color: "white" }}>Customer</Link>
+    </div>
+  );
+}
--- Courier_Frontendd/src/main.jsx
+++ Courier_Frontendd/src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import AddCostumer from './components/AddCustomer.jsx'
-import AddMode from './components/AddMode.jsx'
-
+import Navbar  from './components/NavBar.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AddCostumer />
+    <Navbar />
   </StrictMode>,
 )
