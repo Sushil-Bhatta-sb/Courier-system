5678e9fe6091786e6e029ce20f2246bffa19f272
Sushil <bhattsushil567@gmail.com>
2026-02-09 20:53:09 +0545

    s

--- courier_management/courier_management/settings.py
+++ courier_management/courier_management/settings.py
@@ -50,7 +50,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
     "http://localhost:5173",  
     "http://127.0.0.1:5173",
-    "https://Courier_Frontendd.vercel.app",
+    "https://courier-system-two.vercel.app",
 
 ]
 SESSION_COOKIE_SAMESITE = "None"
