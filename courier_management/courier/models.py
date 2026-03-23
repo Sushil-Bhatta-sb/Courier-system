4056fb19c92eccced996b8403649077eaedbc637
Sushil <bhattsushil567@gmail.com>
2026-03-23 12:10:30 +0545

    ...

--- courier_management/courier/models.py
+++ courier_management/courier/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class cours(models.Model):
+class courier(models.Model):
     name=models.CharField(max_length=200)
     phone=models.CharField(max_length=12)
-    address=models.CharField(max_length=34)
+    address = models.CharField(max_length=255, null=True, blank=True)
     
     def __str__(self):
         return self.name
